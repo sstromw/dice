3ff82110cef587618eb5da584f2082b06937a76c
Sam <sstromw@gmail.com>
2023-10-09 23:21:33 -0500

    better tests

--- ts/test.ts
+++ ts/test.ts
@@ -1,9 +1,17 @@
-import { Coin, Const, D, Geometric, Max, Sum } from "./dice";
+import { Coin, Const, D, Geometric, Max, Roll, Sum } from "./dice";
+
+function log_roll(roll: Roll) {
+    console.log(roll.toString());
+    console.log(`mean: ${roll.mean()}`);
+    console.log(`var : ${roll.variance()}`);
+    console.log(`-----------`);
+    for (let [x,p] of roll.sample_space()) {
+        console.log(`${x}\t${p}`);
+    }
+}
 
 const D4 = new D(4);
 const S = new Sum([D4, D4]);
 const T = new Max([S, S]);
 
-for (let [k,p] of T.sample_space()) {
-    console.log(k, p, T.cdf(k));
-}
+log_roll(T);
