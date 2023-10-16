236ee087ab1c715c8b01c27e6b5e6eea82f60ea3
Sam <sstromw@gmail.com>
2023-10-16 08:37:44 -0500

    file shuffle

--- src/roll.ts
+++ src/roll.ts
@@ -37,3 +37,72 @@ export abstract class Roll {
         return s;
     }
 }
+
+export class D extends Roll {
+    constructor(readonly n: number) {
+        super();
+        this.n = n;
+    }
+    
+    toString() { return `d${this.n}`; }
+
+    roll() {
+        return 1 + Math.floor(Math.random() * this.n);
+    }
+
+    density() {
+        let M = new DefaultMap();
+        for (let k = 1; k <= this.n; k++) {
+            M.set(k, 1/this.n);
+        }
+        return M;
+    }
+
+    mean() { return (this.n + 1)/2; }
+    variance() { return (this.n**2 - 1)/12; }
+}
+
+export class Const extends Roll {
+    constructor(readonly val: number) {
+        super();
+        this.val = val;
+    }
+
+    toString() { return this.val.toString(); }
+    roll() { return this.val; }
+    density() { return new DefaultMap([[this.val, 1]]); }
+    mean() { return this.val; }
+    variance() { return 0; }
+}
+
+export class Geometric extends Roll {
+    constructor(readonly p: number) {
+        super();
+        if (p < 0 || p > 1) {
+            throw new Error("probability out of bounds: " + p);
+        }
+        this.p = p;
+    }
+
+    roll() {
+        let i: number;
+        for(i=1; Math.random() > this.p; i++);
+        return i;
+    }
+
+    density() { return new DefaultMap(); }  // This should never be called
+    sample_space() {
+        if (this._sample_space !== undefined) {
+            return this._sample_space;
+        }
+        this._sample_space = new SampleSpace(
+            (n) => n>0 ? Math.pow(1-this.p, n-1) * this.p: 0,
+        )
+        return this._sample_space;
+    }
+
+    mean() { return 1/this.p; }
+    variance() { return (1-this.p) / this.p**2; }
+
+    toString() { return `G(${this.p})`; }
+}
